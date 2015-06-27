dae25087b714fbe2be783c4d2586b449574fc385
elainegee <elainepgee@gmail.com>
2015-06-27 16:51:41 -0600

    Added sorting functionality.

--- genScripts/VCFrandomizer.py
+++ genScripts/VCFrandomizer.py
@@ -4,6 +4,7 @@ import random
 import logging
 import string
 
+
 __author__='elainegee'
 
 parser = argparse.ArgumentParser(description='Randomly select lines from a VCF.')
@@ -33,24 +34,29 @@ class VCF(object):
             if line[0] == "#":
                 self.header += line
             else:
-                self.variants.append(line)
+                toks = line.split('\t')
+                chrom = toks[0]
+                pos = toks[1]
+                self.variants.append((chrom, pos, line))
 
     def randomizeVCF(self):
-        self.output.write(self.header)
-        for i in range(1, self.nLines + 1):
-            self.output.write(random.choice(self.variants))
+        tmpList = [random.choice(self.variants) for i in range(1, self.nLines + 1)]
+        # sort by chromosome, then by position
+        tmpList = sorted(tmpList, key=lambda x: (x[0],x[1]))
+        outList = [vcfstring for (chrom, pos, vcfstring) in tmpList]
+        self.output.write(self.header + ''.join(outList))
 
 def idGenerator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 logging.basicConfig(format='%(levelname)s: %(asctime)s: %(message)s',
-                        filename="VCFrandomizer-" + idGenerator() + ".log", level=logging.DEBUG)
+                        filename="VCFrandomizer_" + idGenerator() + ".log", level=logging.DEBUG)
 logger = logging.getLogger('VCFrandomizer')
 
 def main():
     if args.number <= 0:
         raise Exception('ERROR: Cannot request non-positive number of output lines.')
-    logger.info('Generating a randomized VCF with ' + str(args.number) + ' lines.')
+    logger.info("Generating a randomized VCF with " + str(args.number) + " lines from '" + args.file.name + "'.")
     vcf = VCF(vcf=args.file, output=args.output, nLines=args.number)
     vcf.readVCF()
     if len(vcf.variants) < args.number:
@@ -58,7 +64,7 @@ def main():
                               'Input VCF only has ' + str(len(vcf.variants)) + ' variants, but '
                               'user requested ' + str(args.number) + ' output variants.')
     vcf.randomizeVCF()
-    logger.info("Randomization completed successfully. Output sent to " + args.output.name)
+    logger.info("Randomization completed successfully.\n--Output sent to '" + args.output.name +"'.\n--DONE.")
 
 
 if __name__ =="__main__":
